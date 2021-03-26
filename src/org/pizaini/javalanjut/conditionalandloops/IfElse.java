267f9c878335f4d262407d0c2f5061488e322e72
Pizaini <zheiner.taurus@gmail.com>
2021-03-26 09:15:36 +0700

    ifelse

--- src/org/pizaini/javalanjut/conditionalandloops/IfElse.java
+++ src/org/pizaini/javalanjut/conditionalandloops/IfElse.java
@@ -1,5 +1,9 @@
 package org.pizaini.javalanjut.conditionalandloops;
 
 public class IfElse {
-
+    public static void main(String[] args) {
+        int x = 10;
+        int y = 5;
+        
+    }
 }
